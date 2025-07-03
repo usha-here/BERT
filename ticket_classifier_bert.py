099e5eeec1672eedbec3aefd72faa4d21d585dda
usha here <127041109+usha-here@users.noreply.github.com>
2025-07-03 09:15:10 +0530

    Added initial BERT ticket classifier with sample labeled data.
    - Added sample complaint dataset with labeled categories (Billing, Outage, Meter)
    - Imported Hugging Face transformers and PyTorch libraries
    - Prepared for model training using BertTokenizer and BertForSequenceClassification.

---
+++ ticket_classifier_bert.py
@@ -0,0 +1,27 @@
+# ticket_classifier_bert.py
+
+import torch    #PyTorch, used for model training and inference.
+import pandas as pd       # For handling tabular dataset.
+from transformers import (   #transformers: Hugging Face library — 
+                            # provides pre-trained models (like BERT), tokenizers, and 
+                            #utilities for training (Trainer, TrainingArguments).
+    BertTokenizer, 
+    BertForSequenceClassification, 
+    Trainer, 
+    TrainingArguments
+)
+
+# 1. Sample Dataset
+data = {
+    'text': [
+        "My electricity bill is too high this month.",
+        "There is a power outage in my area since morning.",
+        "Meter reading is showing wrong value.",
+        "Why is my power bill double this time?",
+        "No electricity in sector 10 since yesterday.",
+        "My meter is not working properly."
+    ],
+    'label': ['Billing', 'Outage', 'Meter', 'Billing', 'Outage', 'Meter']
+}
+df = pd.DataFrame(data)       #  Converts them to a DataFrame using pandas.
+
